cdcb2006c154c7866de3e6bf1533d6729f893bf9
DJ Hartman <danieljayhartman@gmail.com>
2018-01-15 22:35:53 -0500

    Break Lexer out into its own file

---
+++ src/calc5/Lexer.js
@@ -0,0 +1,86 @@
+const {Token, types} = require('./Token')
+const {INT, PLUS, MINUS, MULT, DIV} = types
+
+
+const Lexer = function(text) {
+	this.text = text
+	this.pos = 0
+	this.currentChar = this.text[this.pos]
+}
+
+
+Lexer.prototype.error = function(char) {
+	throw `Unexpected character ${char}`
+}
+
+
+// Increment pos and set currentChar
+Lexer.prototype.advance = function() {
+	this.pos += 1
+	if (this.pos > this.text.length - 1) {
+		this.currentChar = null
+	} else {
+		this.currentChar = this.text[this.pos]
+	}
+}
+
+// Skip whitespace
+Lexer.prototype.skipWhitespace = function() {
+	while (this.currentChar === ' ')
+		this.advance()
+}
+
+// Read one or many digits that make up an integer
+Lexer.prototype.int = function() {
+	let result = ''
+	while (isNumber(this.currentChar)) {
+		result += this.currentChar
+		this.advance()
+	}
+	return Number(result)
+}
+
+
+// Lexical analysis
+Lexer.prototype.getNextToken = function() {
+	while (this.currentChar !== null) {
+		if (this.currentChar === ' ') {
+			this.skipWhitespace()
+			continue
+		}
+
+		if (isNumber(this.currentChar)) {
+			return Token(INT, this.int())
+		}
+
+		if (this.currentChar === '+') {
+			this.advance()
+			return new Token(PLUS)
+		}
+
+		if (this.currentChar === '-') {
+			this.advance()
+			return new Token(MINUS)
+		}
+
+		if (this.currentChar === '*') {
+			this.advance()
+			return new Token(MULT)
+		}
+
+		if (this.currentChar === '/') {
+			this.advance()
+			return new Token(DIV)
+		}
+	} 
+}
+
+
+// Helper function to check if string is a number
+// Remember that isNaN(' ') == false
+function isNumber(str) {
+    return str !== null && !isNaN(str)
+}
+
+
+module.exports.Lexer = Lexer
---
+++ src/calc5/Token.js
@@ -0,0 +1,22 @@
+module.exports.types = {
+	INT: 'INT',
+	PLUS: 'PLUS',
+	MINUS: 'MINUS',
+	MULT: 'MULT',
+	DIV: 'DIV'
+}
+
+
+module.exports.Token = function (type, value) {
+	if (value === undefined) {
+		value = type
+	}
+	this.type = type
+	this.value = value
+}
+
+
+module.exports.Token.prototype.toString = function() {
+	return `Token(${this.type}, ${this.value})`
+}
+
